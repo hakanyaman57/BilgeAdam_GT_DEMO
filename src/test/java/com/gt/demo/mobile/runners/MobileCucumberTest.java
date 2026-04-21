94242c6f33264f2a8dafdfd98980f040199f1574
Hakan Yaman <hakan.yaman@ahlkripto.com>
2026-04-21 13:52:53 +0300

    fix(runners): show VS Code Run button for Cucumber TestNG runners

--- src/test/java/com/gt/demo/mobile/runners/MobileCucumberTest.java
+++ src/test/java/com/gt/demo/mobile/runners/MobileCucumberTest.java
@@ -3,6 +3,7 @@ package com.gt.demo.mobile.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
     features = "classpath:features/mobile",
@@ -15,10 +16,15 @@ import org.testng.annotations.DataProvider;
     tags = "@mobile and not @wip")
 public class MobileCucumberTest extends AbstractTestNGCucumberTests {
 
+  /**
+   * Helps VS Code/TestNG show Run/Debug CodeLens on this runner class in remote environments.
+   */
+  @Test(enabled = false)
+  public void codeLensPlaceholder() {}
+
   @Override
   @DataProvider(parallel = false)
   public Object[][] scenarios() {
     return super.scenarios();
   }
 }
-
--- src/test/java/com/gt/demo/web/runners/WebCucumberTest.java
+++ src/test/java/com/gt/demo/web/runners/WebCucumberTest.java
@@ -3,17 +3,25 @@ package com.gt.demo.web.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
-    features = "classpath:features/web",
+    features = "classpath:features/web/",
     glue = {"com.gt.demo.web"},
     plugin = {
       "pretty",
       "html:target/cucumber/web/cucumber-report.html",
       "json:target/cucumber/web/cucumber.json"
     },
-    tags = "@smoke and @web and not @wip")
+    tags = "@Egitim and @smoke and @web and not @wip")
 public class WebCucumberTest extends AbstractTestNGCucumberTests {
+
+  /**
+   * VS Code/TestNG discovery in some environments expects at least one local @Test method
+   * to show the Run/Debug CodeLens for this class. This placeholder is never executed.
+   */
+  @Test(enabled = false)
+  public void codeLensPlaceholder() {}
 
   @Override
   @DataProvider(parallel = true)
@@ -21,4 +29,3 @@ public class WebCucumberTest extends AbstractTestNGCucumberTests {
     return super.scenarios();
   }
 }
-
